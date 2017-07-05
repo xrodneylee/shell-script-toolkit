2a052b36b44594b422b911163ebabb48caf6d1a5
Lee Guan Pu <xrodneylee@infinitiessoft.com>
2017-07-05 17:30:37 +0800

    installer

---
+++ cloudfusion-patch.sh
@@ -0,0 +1,7 @@
+cd /home/iss-user/tomcatitri
+sudo sh bin/shutdown.sh
+sudo cp webapps/cloudfusion.war backup/cloudfusion_$(date +"%m_%d_%Y_%H_%M_%S")_backup.war
+sudo ls | grep -P "cloudfusion.*" | xargs -d"\n" rm -rf
+sudo cp /tmp/cloudfusion.war webapps/cloudfusion.war
+sudo rm -f  /tmp/cloudfusion.war
+sudo sh bin/startup.sh
---
+++ logloop-install.sh
@@ -0,0 +1,6 @@
+chmod 777 logloop-install-linux64-latest.bin
+./logloop-install-linux64-latest.bin -plogloop@201706
+cd logloop-install
+./install-docker.sh
+./install.sh
+sudo systemctl start logloop
