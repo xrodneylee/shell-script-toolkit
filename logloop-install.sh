4ad40cbb16a6e59fb66f77cc6dd142b74b7b53fa
Lee Guan Pu <xrodneylee@infinitiessoft.com>
2017-07-06 16:57:38 +0800

    logloop install

--- logloop-install.sh
+++ logloop-install.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 # author : guanpu.lee
-
+sudo yum install -y docker-ce-17.06.0.ce-1.el7.centos.x86_64.rpm
+sudo systemctl start docker
 chmod 777 logloop-install-linux64-latest.bin
 ./logloop-install-linux64-latest.bin -plogloop@201706
 cd logloop-install
-./install-docker.sh
 ./install.sh
 sudo systemctl start logloop
+./post-install.sh
+cd ..
+docker cp Caddyfile elk5-cntr:/etc/caddy
+docker exec -it elk5-cntr sv start caddy
