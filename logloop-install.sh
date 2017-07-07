3e030597afade61c56d3a19a135883fdaf3ebde4
Lee Guan Pu <xrodneylee@infinitiessoft.com>
2017-07-07 12:14:26 +0800

    default change logo

--- logloop-install.sh
+++ logloop-install.sh
@@ -13,3 +13,9 @@ sleep 3s
 docker cp Caddyfile elk5-cntr:/etc/caddy
 docker exec -it elk5-cntr sv stop caddy
 docker exec -it elk5-cntr sv start caddy
+docker cp defaults.js elk5-cntr:/opt/kibana/src/ui/settings
+docker cp chrome.jade elk5-cntr:/opt/kibana/src/ui/views
+docker cp ui_app.jade elk5-cntr:/opt/kibana/src/ui/views
+docker cp commons.style.css elk5-cntr:/opt/kibana/optimize/bundles
+docker cp kibanaWelcomeLogo.svg elk5-cntr:/opt/kibana/optimize/bundles
+docker cp logloop-logo.svg elk5-cntr:/opt/kibana/optimize/bundles
