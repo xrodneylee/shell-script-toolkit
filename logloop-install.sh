23e9bc851e49d4d11cad569c2d88ed6e1c9cfc4f
xrodneylee <xrodneylee@gmail.com>
2017-08-03 00:18:51 +0800

    logloop  offline install

--- logloop-install.sh
+++ logloop-install.sh
@@ -7,17 +7,36 @@ DOCKER_FILE="docker-ce-17.06.0.ce-1.el7.centos.x86_64.rpm"
 
 LOGLOOP_FILE="logloop-install-linux64-latest.bin"
 LOGLOOP_PASS="logloop@201706"
+DATA_DIR="/srv/logloop"
+
+DOCKER_VOLUME_LOCAL_PERSIST="local-persist-linux-amd64"
+DOCKER_VOLUME_LOCAL_PERSIST_DEST="/usr/bin/docker-volume-local-persist"
+SYSTEMD_CONFIG_URL="systemd.service"
+SYSTEMD_CONFIG_DEST="/etc/systemd/system/docker-volume-local-persist.service"
 
 function install_docker() {
-    sudo yum install -y $DOCKER_FILE
+    sudo yum install -y ${DOCKER_FILE}
     sudo systemctl start docker
 }
 function create_volume() {
+    mv ${DOCKER_VOLUME_LOCAL_PERSIST} ${DOCKER_VOLUME_LOCAL_PERSIST_DEST}
+    chmod +x ${DOCKER_VOLUME_LOCAL_PERSIST_DEST}
 
+    mv ${SYSTEMD_CONFIG_URL} ${SYSTEMD_CONFIG_DEST}
+
+    sudo systemctl daemon-reload
+    sudo systemctl enable docker-volume-local-persist
+    sudo systemctl start docker-volume-local-persist
+    sudo systemctl status --full --no-pager docker-volume-local-persist
+
+    docker volume create \
+            -d local-persist \
+            -o "mountpoint=${DATA_DIR}" \
+            --name="es_data"
 }
 function install_logloop() {
-    chmod 755 $LOGLOOP_FILE
-    ./$LOGLOOP_FILE -p$LOGLOOP_PASS
+    chmod 755 ${LOGLOOP_FILE}
+    ./${LOGLOOP_FILE} -p${LOGLOOP_PASS}
     cd logloop-install
     ./install.sh
     sudo systemctl start logloop
@@ -41,9 +60,10 @@ function logloop_post_install() {
 }
 function run() {
     install_docker
+    create_volume
     install_logloop
     change_https_to_http
     change_logloop_logo
-    # logloop_post_install
+    logloop_post_install
 }
 run "$@"
