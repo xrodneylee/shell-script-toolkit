a781e8e216aa09e1e35c0226a1d098d3c000ebac
Lee Guan Pu <xrodneylee@infinitiessoft.com>
2017-10-03 15:41:40 +0800

    [bug] wrap problem

---
+++ filter.py
@@ -0,0 +1,7 @@
+
+with open('correct.txt', mode='r') as x, open('error.txt', mode='r') as y:
+    # for temp in y.readlines():
+    #     if temp in x.readlines():
+    #         pass
+    #     else:
+    #         print(temp)
