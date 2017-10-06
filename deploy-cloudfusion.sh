235e785924b588ef4d743731a4982451ba11ad08
Lee Guan Pu <xrodneylee@infinitiessoft.com>
2017-10-06 12:04:24 +0800

    [new] deploy-cloudfusion

---
+++ deploy-cloudfusion.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+function stop_service() {
+    echo "[`date '+%F %T'`]" "INFO: stop service " ${1}
+    systemctl stop ${1}
+    sleep 1
+}
+function start_service() {
+    echo "[`date '+%F %T'`]" "INFO: start service " ${1}
+    systemctl start ${1}
+    sleep 3
+}
+function upgrade() {
+    rm /opt/cloudfusion-Datacenter/webapps/ROOT -rfd
+    rm /opt/cloudfusion-Datacenter/webapps/ROOT.war
+    mv ${1} /opt/cloudfusion-Datacenter/webapps/
+    chmod 0755 /opt/cloudfusion-Datacenter/webapps/ROOT.war
+    chown cloudfusion:cloudfusion /opt/cloudfusion-Datacenter/webapps/ROOT.war
+}
+function run() {
+    stop_service "cloudfusion"
+    upgrade /tmp/ROOT.war
+    start_service "cloudfusion"
+}
+run "$@"
