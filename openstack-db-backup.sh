050b9b3aee0e33c209f4e703d861b755a771a061
Lee Guan Pu <xrodneylee@infinitiessoft.com>
2018-04-19 12:12:10 +0800

    openstack-db-backup

--- openstack-db-backup.sh
+++ openstack-db-backup.sh
@@ -5,10 +5,10 @@ function run() {
 
     echo "[`date '+%F %T'`]" "INFO: ssh remote"
     echo "[`date '+%F %T'`]" "INFO: start backup..."
-    ssh controller02 bash -c "'
+    ssh ${1} bash -c "'
     su - postgres -c pg_dumpall > ~/$filename
     '"
     echo "[`date '+%F %T'`]" "INFO: finished backup..."
-    scp controller02:~/$filename /root/infinitiessoft/openstack-db-backup/
+    scp ${1}:~/$filename /root/infinitiessoft/openstack-db-backup/
 }
 run "$@"
